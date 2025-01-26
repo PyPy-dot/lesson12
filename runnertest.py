b95aef584a04091a55400c62e0572b8f3a517e11
PyPy-dot <nerosed131@gmail.com>
2025-01-26 15:59:41 +0500

    1

---
+++ runnertest.py
@@ -0,0 +1,25 @@
+import unittest
+import runner
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        walker = runner.Runner('Fill')
+        for i in range(10):
+            walker.walk()
+        self.assertEqual(walker.distance, 50)
+
+    def test_run(self):
+        runner_ = runner.Runner('Fill')
+        for i in range(10):
+            runner_.run()
+        self.assertEqual(runner_.distance, 100)
+
+    def test_challenge(self):
+        run1 = runner.Runner('Fill')
+        run2 = runner.Runner('Gabe')
+        for i in range(10):
+            run1.run()
+            run2.walk()
+        self.assertNotEqual(run1.distance, run2.distance)
+
